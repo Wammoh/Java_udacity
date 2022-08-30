f2895ee27b1eaa2a4354f5bce79b0401da459cc8
TroyJohnson <53130184+Wammoh@users.noreply.github.com>
2022-08-30 11:56:09 -0500

    java class/coding with Mosh

--- src/com/troy/Main.java
+++ src/com/troy/Main.java
@@ -8,36 +8,36 @@ import java.util.Date;
 
 public class Main {
     public static void  main(String[] args){
-//        byte age = 30;
-//        Date now = new Date();
-//        System.out.println(now);
-//        System.out.println(age);
-//        System.out.println("Main.main");
-//        String message = "  Fun with Java, \"compilation error\"  " + "!!"; //Methods escape the double quote
-//        System.out.println(message.endsWith("!!"));
-//        //c:\\Windows\\...  using \ to escape
-//        System.out.println("This is\n cool!");
-//        System.out.println("This is\t cool!");
-//        System.out.println(message);
-//        System.out.println(message.replace("!", "*"));
-//        System.out.println(message.indexOf("fun")); //find the index
-//        System.out.println(message.length());// used to find length of the message
-//        System.out.println(message); // Strings are immutable!
-//        System.out.println(message.trim());// gets rid of whitespace
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+        System.out.println(age);
+        System.out.println("Main.main");
+        String message = "  Fun with Java, \"compilation error\"  " + "!!"; //Methods escape the double quote
+        System.out.println(message.endsWith("!!"));
+        //c:\\Windows\\...  using \ to escape
+        System.out.println("This is\n cool!");
+        System.out.println("This is\t cool!");
+        System.out.println(message);
+        System.out.println(message.replace("!", "*"));
+        System.out.println(message.indexOf("fun")); //find the index
+        System.out.println(message.length());// used to find length of the message
+        System.out.println(message); // Strings are immutable!
+        System.out.println(message.trim());// gets rid of whitespace
         // ARRAYS//
         // in Java Arrays have a fixed size, once made they can not be changed.
         //you can sort an array Single  arrays
-//        int[] numbers = new int[5];
-//        numbers[0] = 1;
-//        numbers[1] = 2;
-//        numbers[2] = 3;
-//        System.out.println(numbers);
-//        System.out.println(Arrays.toString(numbers));
-//        int[] numbers2 ={2,3,4,5,6,7,8};
-//        Arrays.sort(numbers2);
-//        System.out.println(numbers2);
-//        System.out.println(numbers2.length);
-//
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println(numbers);
+        System.out.println(Arrays.toString(numbers));
+        int[] numbers2 ={2,3,4,5,6,7,8};
+        Arrays.sort(numbers2);
+        System.out.println(numbers2);
+        System.out.println(numbers2.length);
+
 
 
 
